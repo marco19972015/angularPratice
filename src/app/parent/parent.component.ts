1c80741f78641261049959fc0c4eb6a0908491c5
Marco Torres <marcoantonio.torres@icloud.com>
2023-10-12 10:29:07 -0400

    Added an object to our component and used string interpolation in our template to call it. Created a method that returned the discounted price for the item. Used ternary operator in combination with string interpolation in our template to show a specific template

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -11,6 +11,21 @@ export class ParentComponent{
   firstName: string = 'John';
   lastName: string = 'Smith';
   age: number = 25
+
+  product = {
+    phoneBrand: 'Apple',
+    phoneType: 'iPhone 12',
+    phoneSerial: 1010101,
+    price: 800,
+    discount: .2,
+    inStock: 5
+  }
+
+  getDiscount() {
+    const discount = this.product.price * this.product.discount;
+    const discountedPrice = this.product.price - discount;
+    return(discountedPrice);
+  }
   
 }
  
