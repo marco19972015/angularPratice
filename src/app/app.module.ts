e15fe5c04348d5ee942ed472f386ec7ce6e5162f
Marco Torres <marcoantonio.torres@icloud.com>
2023-10-12 16:32:35 -0400

    Added two way binding through the use of ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ParentComponent } from './parent/parent.component';
@@ -11,10 +12,12 @@ import { SearchComponent } from './parent/search/search.component';
     AppComponent,
     ParentComponent,
     ChildComponent,
-    SearchComponent
+    SearchComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/parent/search/search.component.ts
+++ src/app/parent/search/search.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+  searchText: string = 'Mens wear';
 
+  // updateSearchText(event: any) {
+  //   this.searchText = event.target.value;
+  //  }
 }
