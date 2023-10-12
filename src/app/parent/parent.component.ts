4373c839d45e107ba2225e59c1bf64b2868921f5
Marco Torres <marcoantonio.torres@icloud.com>
2023-10-12 13:31:57 -0400

    added event binding

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -29,6 +29,16 @@ export class ParentComponent{
     const discountedPrice = this.product.price - discount;
     return(discountedPrice);
   }
+
+  // This method is associated with the event binding in our template
+  onNameChange(event: any) {
+    // We can access the event object
+    console.log(event);
+    // From there we can navigate to get the value contained in the event
+    console.log(event.target.value);
+    // We can then set the value in our input = to our property randomName 
+    this.randomName = event.target.value;
+  }
   
 }
  
